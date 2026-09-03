9ccb0be915f5b4604a5128aec1e516e741190159
garvsingh61 <garv.singhh61@gmail.com>
2026-09-03 15:35:37 +0530

    Update Student Result program

--- StudentResult.java
+++ StudentResult.java
@@ -1,25 +1,27 @@
-import java.util.*;
+import java.util.Scanner;
 
 class StudentResult {
 
-    public static void main(String[] args) {
+    String name;
+    int total;
+    int per;
 
-        Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
 
+    void input() {
         System.out.print("Enter name: ");
-        String name = sc.nextLine();
+        name = sc.nextLine();
 
         System.out.print("Enter 5 marks: ");
+        total = sc.nextInt() + sc.nextInt() + sc.nextInt()
+              + sc.nextInt() + sc.nextInt();
+    }
 
-        int total = sc.nextInt() + sc.nextInt() + sc.nextInt()
-                  + sc.nextInt() + sc.nextInt();
+    void percentage() {
+        per = total / 5;
+    }
 
-        int per = total / 5;
-
-        System.out.println("\nName: " + name);
-        System.out.println("Total: " + total);
-        System.out.println("Percentage: " + per + "%");
-
+    void grade() {
         if (per >= 90)
             System.out.println("Grade: A+");
         else if (per >= 80)
@@ -32,10 +34,29 @@ class StudentResult {
             System.out.println("Grade: D");
         else
             System.out.println("Grade: F");
+    }
 
+    void result() {
         if (per >= 50)
             System.out.println("Result: PASS");
         else
             System.out.println("Result: FAIL");
+    }
+
+    void display() {
+        System.out.println("\nName: " + name);
+        System.out.println("Total: " + total);
+        System.out.println("Percentage: " + per + "%");
+    }
+
+    public static void main(String[] args) {
+
+        StudentResult s = new StudentResult();
+
+        s.input();
+        s.percentage();
+        s.display();
+        s.grade();
+        s.result();
     }
 }
